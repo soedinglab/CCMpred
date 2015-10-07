b2c8c3aff881cceb2c37b92c1208c8171c1f9c25
Stefan Seemayer <stefan@seemayer.de>
2015-10-07 15:31:31 +0200

    Add parallelization to reweighting

--- src/reweighting.c
+++ src/reweighting.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "ccmpred.h"
 #include "reweighting.h"
 
@@ -10,20 +11,32 @@ void calculate_weights(conjugrad_float_t *w, unsigned char *msa, int ncol, int n
 
 	memset(w, 0, sizeof(conjugrad_float_t) * nrow);
 
-	for(int i = 0; i < nrow; i++) {
-		for(int j = i+1; j < nrow; j++) {
+	uint64_t nij = nrow * (nrow + 1) / 2;
 
-			int ids = 0;
-			for(int k = 0; k < ncol; k++) {
-				if(msa[msa_index(i, k)] == msa[msa_index(j, k)]) {
-					ids++;
-				}
-			}
+	#pragma omp parallel
+	#pragma omp for nowait
+	for(uint64_t ij = 0; ij < nij; ij++) {
 
-			if(ids > idthres) {
-				w[i]++;
-				w[j]++;
+		// compute i and j from ij
+		// http://stackoverflow.com/a/244550/1181102
+		uint64_t i, j;
+		{
+			uint64_t ii = nrow * (nrow + 1) / 2 - 1 - ij;
+			uint64_t K = floor((sqrt(8 * ii + 1) - 1) / 2);
+			i = nrow - 1 - K;
+			j = ij - nrow * i + i * (i + 1) / 2;
+	 	}
+
+		int ids = 0;
+		for(int k = 0; k < ncol; k++) {
+			if(msa[msa_index(i, k)] == msa[msa_index(j, k)]) {
+				ids++;
 			}
+		}
+
+		if(ids > idthres) {
+			w[i]++;
+			w[j]++;
 		}
 	}
 
