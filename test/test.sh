c4e5cf209a9fb8892459738b9fd20065b06bdf69
Stefan Seemayer <stefan@seemayer.de>
2016-01-12 09:02:18 +0100

    Disable CUDA for tests

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ source_dir=$PWD
 temp_dir=`mktemp -d`
 
 cd $temp_dir
-cmake $source_dir
+cmake $source_dir -DWITH_CUDA=off
 make
 bin/ccmpred $source_dir/example/1atzA.aln 1atzA.mat
 [[ $(wc -l < 1atzA.mat) == 75 ]]
